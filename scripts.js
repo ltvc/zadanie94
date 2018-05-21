ba24b58b6196eb1fbbb80d4844cfd656221aa964
ltvc <lataviec666@gmail.com>
2018-05-21 22:37:12 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+var ile;
+function drawTree (ile){
+	for (var i=0; i<ile; i++) {
+		var star='';
+		var space='';
+		for (var j=0; j<i; j++){
+			var star=star += '*';
+			}
+			console.log(star);
+		
+}
+}
+
+	drawTree(12);
